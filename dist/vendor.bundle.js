d468520d4b19fab712af7a66e35b243b9a1668af
calmeii <hk_huang_kun@163.com>
2017-02-24 19:45:48 +0800

    effe

--- dist/vendor.bundle.js
+++ dist/vendor.bundle.js
@@ -86,7 +86,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "1a56e5cd85e02724026e"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "096f597c3d53b309f410"; // eslint-disable-line no-unused-vars
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotMainModule = true; // eslint-disable-line no-unused-vars
 /******/ 	var hotCurrentParents = []; // eslint-disable-line no-unused-vars
