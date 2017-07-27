b93673492836f1727a0e6645d26a614845019504
magallance <lcandzq@126.com>
2017-07-27 16:40:04 +0800

    修改今日满房的点距离

--- src/page/one/components/room-piece/index.js
+++ src/page/one/components/room-piece/index.js
@@ -44,12 +44,12 @@ class RoomPiece extends React.Component {
         const getStockNotiy = () => {
             if(booked == num) {
                 return (
-                    <div className="rest_error">.今日满房</div> 
+                    <div className="rest_error">· 今日满房</div> 
                 )
             }
             if(booked/num >= 0.8) {
                 return (
-                    <div className="rest_waring">·今日仅剩{num - booked}间</div>
+                    <div className="rest_waring">· 今日仅剩{num - booked}间</div>
                 )
             }
         }
